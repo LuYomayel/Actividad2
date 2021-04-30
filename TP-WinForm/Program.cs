7f2362cecd6bfefd38946eec0c5b651ff7c79e61
msarfernandez <msarfernandez@gmail.com>
2021-04-30 19:32:04 -0300

    agregué un comentario inutil...

--- TP-WinForm/Program.cs
+++ TP-WinForm/Program.cs
@@ -14,6 +14,7 @@ namespace Presentacion
         [STAThread]
         static void Main()
         {
+            // COMENTARIO DE MAXI
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
