951da4c6a7f86527d2e96a39ce8af4ff7eb19305
Herrera Alejandro <81126261+aleh07@users.noreply.github.com>
2021-04-30 20:27:25 -0300

    pimer commit

--- TP-WinForm/Form1.cs
+++ TP-WinForm/Form1.cs
@@ -20,7 +20,7 @@ namespace Presentacion
         {
             InitializeComponent();
         }
-
+        //comentario prueba
         private void button3_Click(object sender, EventArgs e)
         {
             Producto seleccionado = (Producto)dgw.CurrentRow.DataBoundItem;
